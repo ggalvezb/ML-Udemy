dbdbeefa7c119869b202bc70792b123c4455a293
ggalvezb <32131902+ggalvezb@users.noreply.github.com>
2020-02-15 19:39:44 -0300

    Suport Vector Regression

---
+++ Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/svr_my.py
@@ -0,0 +1,52 @@
+# SVR
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values.reshape(-1,1)
+
+# Splitting the dataset into the Training set and Test set
+"""from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+sc_y = StandardScaler()
+X = sc_X.fit_transform(X)
+y = sc_y.fit_transform(y)
+
+# Fitting SVR to the dataset
+from sklearn.svm import SVR
+regressor = SVR(kernel = 'rbf') #This is gaussian kernel
+regressor.fit(X, y)
+
+# Predicting a new result
+to_predict=np.arange(1).reshape(-1,1)
+to_predict=to_predict.astype('float64')
+to_predict[0][0]=6.5
+y_pred = regressor.predict(sc_X.transform(to_predict)) #other option is make to_predict=np.arra([[6.5]])
+y_pred = sc_y.inverse_transform(y_pred) #here we aplay a invert transform to back a normal scale
+
+# Visualising the SVR results
+plt.scatter(X, y, color = 'red')
+plt.plot(X, regressor.predict(X), color = 'blue')
+plt.title('Truth or Bluff (SVR)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+# Visualising the SVR results (for higher resolution and smoother curve)
+X_grid = np.arange(min(X), max(X), 0.01) # choice of 0.01 instead of 0.1 step because the data is feature scaled
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.title('Truth or Bluff (SVR)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
